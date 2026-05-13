bb732dc50a8568002c97c40bcb38655c94eddc46
erenmemba10-stack <erenmemba10@gmail.com>
2026-05-13 13:33:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+window.addEventListener("load", () => {
+  document.body.classList.add("loaded");
+});
+
+function toggleMenu(){
+  document.getElementById("navLinks").classList.toggle("active");
+}
